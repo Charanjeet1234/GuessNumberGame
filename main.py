bfff7de3398c66c12ac6c635c7eb2fec545e7da5
Charan <cswatermarc@gmail.com>
2022-06-01 12:54:31 +0400

    Added user number and compare

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import random
 
-
+# Take the random integer
 randomno = random.randint(1,50)
 print(randomno)
+guesses = 0
+userguess = None
+
+while(userguess != randomno):
+    userguess = int(input("Enter your guess: "))
+    guesses += 1
+
+    if userguess == randomno:
+        print("***************************************************")
+        print("You have guessed correct")
+        print("You gussed the correct answer in:", guesses, "Attempt")
+        print("***************************************************")
+
+    else:
+        if userguess > randomno:
+            print("Please Enter Lower Number ")
+        else:
+            print("Please Enter Higher Number ")
+
+
+
+# Count Gusses
+
+    
